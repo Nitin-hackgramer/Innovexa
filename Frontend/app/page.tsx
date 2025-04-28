b5ff9461399d831f1b2955491ef3a924de6428b4
Nitin Sharma <108229177+Nitin-hackgramer@users.noreply.github.com>
2025-04-28 18:37:29 +0530

    Update page.tsx

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -10,7 +10,7 @@ import Footer from '@/components/footer';
 
 export default function Home() {
   return (
-    <main className="pt-20">
+    <main>
       <HeroSection />
       <AboutSection />
       <GroupsSection />
