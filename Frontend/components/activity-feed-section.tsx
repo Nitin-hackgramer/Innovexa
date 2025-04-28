487ffd799793102696012b34f90c558762801cf9
Nitin Sharma <108229177+Nitin-hackgramer@users.noreply.github.com>
2025-04-28 19:11:55 +0530

    Update activity-feed-section.tsx

--- Frontend/components/activity-feed-section.tsx
+++ Frontend/components/activity-feed-section.tsx
@@ -51,7 +51,10 @@ const activities = [
 
 export default function ActivityFeedSection() {
   return (
-    <section id="activity" className="py-20 px-4 bg-muted/50">
+    <section id="activity" className="relative py-20 px-4 bg-muted/50">
+      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[93%] h-1">
+          <div className="absolute inset-0 bg-gradient-to-r from-orange-400 via-purple-500 to-blue-600"></div>
+      </div>
       <div className="max-w-7xl mx-auto">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
