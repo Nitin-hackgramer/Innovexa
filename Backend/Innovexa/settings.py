5633ad5a17e859a85c6b522ebe71b3dbfccc1ea8
Nitin Sharma <108229177+Nitin-hackgramer@users.noreply.github.com>
2025-04-29 20:16:57 +0530

    Update settings.py

--- Backend/Innovexa/settings.py
+++ Backend/Innovexa/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -64,7 +65,7 @@ DATABASES = {
     }
 }
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 AUTH_PASSWORD_VALIDATORS = []
 
