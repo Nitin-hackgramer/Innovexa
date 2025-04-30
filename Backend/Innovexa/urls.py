ad606a01ccad2035c165d7ed9e7f1fefa48fcb48
Nitin Sharma <108229177+Nitin-hackgramer@users.noreply.github.com>
2025-04-30 07:42:58 +0530

    Update urls.py

--- Backend/Innovexa/urls.py
+++ Backend/Innovexa/urls.py
@@ -20,7 +20,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('contact.urls')),
-    path('api/contact/', include('contact.urls')),
+   path('api/', include('contact.urls')),
 ]
 urlpatterns += staticfiles_urlpatterns()
