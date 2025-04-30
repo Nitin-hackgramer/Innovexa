92c8d6c71d39f8e574e6dcca64a661d9b21ecd51
Nitin Sharma <108229177+Nitin-hackgramer@users.noreply.github.com>
2025-04-30 07:42:21 +0530

    Update urls.py

--- Backend/contact/urls.py
+++ Backend/contact/urls.py
@@ -1,6 +1,8 @@
 
 from django.urls import path
 from .views import contact_view
+from django.http import JsonResponse
+from django.http import HttpRequest  
 
 def root_view(request):
     return JsonResponse({"message": "Welcome to the API!"})
